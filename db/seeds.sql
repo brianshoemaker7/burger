d50c80f9c006facea2ee462f5070ba205ce5fea8
brianshoemaker7 <brianshoemaker7@gmail.com>
2016-07-01 17:09:35 -0400

    Initial files upload

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', true);
